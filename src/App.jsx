004d5102d8c4670a14cb336dd21024ef598462c1
Sergeypervak <har4enko1983@gmail.com>
2022-07-05 16:24:03 +0300

    two states

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React,{useState} from "react";
 
 function App() {
   const [count, setCount] = useState (0);
+  const [loading, setLoading] = useState (false);
  
  const increment = () => {
   setCount(count + 1)
