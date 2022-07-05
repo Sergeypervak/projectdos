686636ea1a648297b7744a4ccf0423c910dbe141
Sergeypervak <har4enko1983@gmail.com>
2022-07-05 16:18:23 +0300

    counter with hooks functions

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React,{useState} from "react";
 
 function App() {
-  const [count, setCount] = useState (8);
+  const [count, setCount] = useState (0);
  
- 
+ const increment = () => {
+  setCount(count + 1)
+ }
  
   return (
     <div className="App">
-      <button onClick={() =>setCount(count + 1) }>{count}</button>
+      <button onClick={increment}>{count}</button>
     </div>
   );
 }
