49a6d3aa6496765e37bd783bc64d2f490f0d76e1
Sergeypervak <har4enko1983@gmail.com>
2022-07-08 23:29:41 +0300

    reff

--- src/Ref.jsx
+++ src/Ref.jsx
@@ -1,19 +1,28 @@
 import React,{useEffect, useRef} from "react";
 
 const WithRef = () => {
-const inputEl = useRef(null);
-console.log(inputEl);    
+// const inputEl = useRef(null);
+// console.log(inputEl);    
+
+
+const numRef = useRef(0)
+
+const handleClick = () => {
+numRef.current = 1;
+
+} 
 
 useEffect(() => {
-    console.log(inputEl);
+    // console.log(inputEl);
+   console.log(numRef.current); 
 });
 
 return (
    
    
    <div>
-        <input placeholder="name" ref={inputEl} />
-      
+        {/* <input placeholder="name" ref={inputEl} /> */}
+      <button onClick={handleClick}>{numRef.current}</button>
     </div>
 );
 };
