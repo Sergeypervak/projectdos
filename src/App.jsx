49993a714b802c79be6f068a785692c4396df100
Sergeypervak <har4enko1983@gmail.com>
2022-07-12 00:31:39 +0300

    timer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React,{useState} from "react";
-import Clicker from "./Clicker";
-import WithRef from "./Ref";
+
+import
 
 
 function App() {
---
+++ src/Timer.jsx
@@ -0,0 +1,17 @@
+import React,{useState, useEffect, useRef} from "react";
+
+
+export default function Timer () {
+    const [] = useState(0);
+    const[isCounting, setIsCount] = useState(false);
+
+    const handleReset = () => {
+        this.setState({
+            count: 0,
+            isCounting: false,
+        });
+
+    };
+
+
+};
