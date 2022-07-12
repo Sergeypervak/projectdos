b4111ffa64143b13fb83d9edd7634354d26fd981
Sergeypervak <har4enko1983@gmail.com>
2022-07-12 23:34:19 +0300

    timer pro

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -2,7 +2,7 @@ import React,{useState, useEffect, useRef} from "react";
 
 
 export default function Timer () {
-    const [] = useState(0);
+    const [count, setCount] = useState(0);
     const[isCounting, setIsCount] = useState(false);
 
     const handleReset = () => {
@@ -10,8 +10,25 @@ export default function Timer () {
             count: 0,
             isCounting: false,
         });
+     };
+    const handleStart = () => {
+        this.setState({
+            isCounting: false,
+    });
+    this.counterId = setInterval(() => {
+        this.setState({count: this.state.count + 1});
+    }, 1000);
+};
 
-    };
+const handleStop = () => {
+    clearInterval(this.counterId);
+    this.setState({
+        isCounting: false,
+    });
+};
+
+
+
 
 
 };
