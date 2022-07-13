733ac513687b558195ff27f578768da147d31d61
Sergeypervak <har4enko1983@gmail.com>
2022-07-13 23:38:33 +0300

    cycle

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -1,34 +1,47 @@
 import React,{useState, useEffect, useRef} from "react";
 
+function setDefaultValue() {
+    
+}
 
 export default function Timer () {
     const [count, setCount] = useState(0);
     const[isCounting, setIsCount] = useState(false);
-
+    const timerIdRef=useRef(null);
     const handleReset = () => {
-        this.setState({
-            count: 0,
-            isCounting: false,
-        });
+        setCount(0);
+        setIsCount(false);
+       };
      };
     const handleStart = () => {
-        this.setState({
-            isCounting: false,
-    });
-    this.counterId = setInterval(() => {
-        this.setState({count: this.state.count + 1});
+      setIsCount(true)
+      timerIdRef.current = setInterval(() => {
+        setCount(count + 1)
     }, 1000);
 };
 
 const handleStop = () => {
-    clearInterval(this.counterId);
-    this.setState({
-        isCounting: false,
-    });
+    clearInterval(timerIdRef.current);
+   setIsCount(false);
 };
 
+useEffect(() => {
+
+}, [count]);
+
+return (
+    <div className="timer">
+        <h1>React timer</h1>
+        <h3>{count}</h3>
+        {!isCounting ? (
+            <button onClick={handleStart}>Start</button>
+        ) : (
+            <button onClick={handleStop}>Stop</button>
+        )}
+        <button onClick={handleReset}>Reset</button>
+    </div>
+)
 
 
 
-
-};
+;
